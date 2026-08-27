e465ab4a5fd4ab7b611289b48bca6970a1290ba8
André Mussato <andre.mussato@gmail.com>
2026-08-27 08:34:12 -0300

    Aula do dia 27-08

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,3 +5,35 @@ import React, { useState, useRef } from 'react'
 
 //Importa o arquivo CSS específico do componente Game
 import './Game.css'
+
+//Cria o componente Game (Jogo)
+//O componente recebe várias propriedades (props) vindas do App.jsx
+function Game({
+    verifyLetter, //verifyLetter = verifica letra
+    pickedWord, //pickedWord = palavra escolhida/sorteada
+    pickedCategory, //pickedCategory = categoria escolhida/sorteada
+    letters, //letters = letras da palavra
+    guessedLetters, //guessedLetters = letras advinhadas/corretas
+    wrongLetters, //wrongLetters = letras erradas
+    guesses, //guesses = tentativas
+    score, //score = pontuação
+}) {
+
+    //Cria um estado que armazena a letra digitada pelo usuário
+    //letter = letra
+    //setLetter = função utilizada para alterar a letra
+    const [letter, setLetter] = useState("");
+
+    //Cria uma referência para o campo input
+    //letterInputRef = referência do input da letra
+    //Essa referência será utilizada posteriormente para
+    //colocar o foco novamente no input
+    const letterInputRef = useRef(null);
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Game
