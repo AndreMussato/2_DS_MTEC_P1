e28d851c4affcfb59f00d4906414a8eed45a1b09
André Mussato <andre.mussato@gmail.com>
2026-08-27 07:59:48 -0300

    first commit

---
+++ src/components/Game.jsx
@@ -0,0 +1,7 @@
+//Importa o React e os Hooks:
+// useState = permite criar e alterar estados
+// useRef = permite criar uma referência direta para um elemento HTML
+import React, { useState, useRef } from 'react'
+
+//Importa o arquivo CSS específico do componente Game
+import './Game.css'
---
+++ src/data/words.js
@@ -0,0 +1,9 @@
+export const wordList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Volante", "Cinto", "Parabrisa"],
+    fruta: ["Banana", "Maçã", "Pera", "Mamão", "Laranja", "Caju", "Melão", "Jaca"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Pé", "Orelha", "Nariz"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador", "CPU", "Dissipador", "Memória"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Vite", "Java", "CSS", "Código"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Bolo", "Pão", "Torta", "Mousse", "Pizza"],
+    animal: ["Gato", "Cachorro", "Pato", "Cabrito", "Macaco", "Lagartixa", "Onça", "Cervo"],
+}
